1a842d1685fce93e6c03ed309206ef71db478c4f
sina jamshdi <>
2021-05-23 22:31:42 +0430

    working on frontend

--- game/views.py
+++ game/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render,redirect
+from django.http import Http404
 
 
 def index(request):
@@ -9,4 +10,14 @@ def index(request):
             '/play/%s?&choice=%s' 
             %(room_code, char_choice) #what happen here ?
         )
-    return render(request, "index.html", {})
+    return render(request, "game/index.html", {})
+
+def game(request, room_code):
+    choice = request.GET.get("choice")
+    if choice not in ['X', 'O']:
+        raise Http404("Choice does not exists")
+    context = {
+        "char_choice": choice, 
+        "room_code": room_code
+    }
+    return render(request, "game/game.html", context)
--- tic_tac_toe/urls.py
+++ tic_tac_toe/urls.py
@@ -2,9 +2,10 @@
 from django.contrib import admin
 from django.urls import path
 # from game.views import index
-from django_websocket.game import index
+from game import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path("",index,name="home")
+    path("",views.index,name="home"),
+    path('play/<room_code>', views.game),
 ]
